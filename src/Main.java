83827996cc25d94a11e65535b380f60401dc703f
guashasha <davidcarrionr00@hotmail.com>
2024-10-16 12:21:14 -0600

    creación de fxml pantalla principal

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
-package storyteller;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.application.Application;
-
-import java.io.IOException;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -16,6 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader guiLoader = new FXMLLoader(getClass().getResource("storyteller/view/Start.fxml"));
+        System.out.println(getClass().getResource("/src/storyteller/view/Start.fxml"));
 
         try {
             Parent root = guiLoader.load();
@@ -24,7 +21,7 @@ public class Main extends Application {
             stage.setScene(scene);
             stage.show();
         }
-        catch (IOException error) {
+        catch (Exception error) {
             error.printStackTrace();
         }
     }
